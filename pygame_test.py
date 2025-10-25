1ccc89df12b9a0a250f3cedd98799e05fd4475ca
Noapoleon <noalegrand088@gmail.com>
2025-10-25 23:19:47 +0200

    added pyinstaller build to workflow, python deps files and renamed script

--- pygame_test.py
+++ pygame_test.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
 import pygame
+import time
 
 def main(width: int, height: int):
     # init pygame
     pygame.init()
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Game Test")
+    font = pygame.font.SysFont(None, 30)
 
     # clock for FPS control
     clock = pygame.time.Clock()
 
     # loop
+    fps = 0
+    frames = 0
+    last = time.time()
     running = True
     while running:
         # Event handling
@@ -22,21 +27,25 @@ def main(width: int, height: int):
         # get current mouse position
         x, y = pygame.mouse.get_pos()
 
-
         # --- Drawing ---
         # Fill screen with dark grey
         screen.fill((30,30,30))
         # Draw circle as cyan circle
         pygame.draw.circle(screen, (0, 200, 255),  (x, y), 10)
+        # fps counter
+        frames += 1
+        if (time.time() - last >= 1.0):
+            fps = frames
+            frames = 0
+            last = time.time()
+        fps_text = font.render(f"FPS: {fps}", True, (255, 255, 255))
+        screen.blit(fps_text, (10, 10))
 
-
+        # Limit fps
+        # clock.tick(60)
 
         # Update display
         pygame.display.flip()
-
-        # Limit to 60 frames per second
-        # clock.tick(60)
-
     pygame.quit()
 
 
