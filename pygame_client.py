d000dbaef0ada3a25a3f4dd9cc708f4ba0b36fce
Noapoleon <noalegrand088@gmail.com>
2025-10-28 08:03:24 +0100

    removed print in client, added support for ip config file

--- pygame_client.py
+++ pygame_client.py
@@ -6,11 +6,19 @@ import asyncio
 import websockets
 import json
 
-SERVER_URI = "ws://127.0.0.1:8765"
-
-
 async def main():
-    async with websockets.connect(SERVER_URI) as websocket:
+    server_ip = ""
+    try:
+        with open("config", "r") as f:
+            server_ip = f.readline().strip()
+            if not server_ip:
+                raise ValueError("Config file is empty.")
+            print(f"Loaded server IP from config: {server_ip}")
+    except FileNotFoundError:
+        print("Config file not found.")
+    except Exception as e:
+        print(f"Error reading config file: {e}")
+    async with websockets.connect(f"ws://{server_ip}") as websocket:
         print("\033[32mConnected to server\033[0m")
 
         pygame.init()
@@ -37,7 +45,6 @@ async def main():
             nonlocal players
             async for message in websocket:
                 players = json.loads(message)
-                print(players)
 
         asyncio.create_task(send_position())
         asyncio.create_task(receive_positions())
